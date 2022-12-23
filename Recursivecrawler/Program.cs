5afd6839e4a668c93d414f77378f72f5a7320b72
PsykoDev <johann.l_62@hotmail.fr>
2022-12-23 03:07:02 +0100

    hum

--- Recursivecrawler/Program.cs
+++ Recursivecrawler/Program.cs
@@ -11,7 +11,7 @@ namespace Recursivecrawler
         static HttpClient _httpclient = new();
         static Regex rx = new Regex(@".*\.(jpg|png|gif|jpeg|avif|webp)?$");
         static Mutex s = new();
-
+        static List<string> alreadysee = new();
         static ConcurrentQueue<string> cq = new ConcurrentQueue<string>();
 
 
@@ -19,7 +19,17 @@ namespace Recursivecrawler
         static async Task Main(string[] args)
         {
 
-           //await ParseLinksAsync("https://www.wallpaperflare.com/");
+            for (int i = 0; i < 15; i++)
+            {
+                ThreadPool.QueueUserWorkItem(new WaitCallback(delegate (object state) { process(); }), null);
+            }
+
+            Console.Read();
+        }
+
+
+        static async void process()
+        {
 
             HttpResponseMessage resp = await _httpclient.GetAsync("https://www.wallpaperflare.com/");
 
@@ -36,22 +46,29 @@ namespace Recursivecrawler
             {
                 string href = n.Attributes["href"].Value;
                 cq.Enqueue(GetAbsoluteUrlString("https://www.wallpaperflare.com/", href));
+                alreadysee.Add(GetAbsoluteUrlString("https://www.wallpaperflare.com/", href));
             }
 
 
             Action action = () =>
             {
                 string dequeued;
+                Console.WriteLine("Trying");
                 while (true)
                 {
                     if (cq.TryDequeue(out dequeued))
                     {
+                        Console.WriteLine("processing queue");
                         var document = new HtmlWeb().Load(dequeued);
                         var urls = document.DocumentNode.Descendants("img").Select(e => e.GetAttributeValue("src", null)).Where(s => !String.IsNullOrEmpty(s));
                         foreach (var img in urls)
                         {
-                            if(rx.IsMatch(img))
-                                Console.WriteLine(img);
+                            if (rx.IsMatch(img))
+                            {
+
+                                Console.WriteLine(img + "\n");
+                            }
+                            Console.WriteLine(cq.Count);
                         }
                         ParseLinksAsync(dequeued);
 
@@ -59,8 +76,8 @@ namespace Recursivecrawler
                 }
             };
 
-            Parallel.Invoke(action, action, action, action, action, action, action, action, action, action, action, action, action, action, action, action);
-            Console.Read();
+            for (int i = 0; i < 15; i++)
+                Parallel.Invoke(action);
         }
 
 
@@ -82,7 +99,15 @@ namespace Recursivecrawler
             foreach (var n in nodes)
             {
                 string href = n.Attributes["href"].Value;
-                cq.Enqueue(GetAbsoluteUrlString(urlToCrawl, href));
+                //cq.Enqueue(GetAbsoluteUrlString(urlToCrawl, href));
+                alreadysee.Add(GetAbsoluteUrlString(urlToCrawl, href));
+                RemoveDuplicatesSet(alreadysee);
+                cq.Clear();
+                for(int i = 0; i<alreadysee.Count; i++)
+                {
+                    cq.Enqueue(alreadysee[i]);
+                }
+                
             }
         }
 
@@ -92,6 +117,21 @@ namespace Recursivecrawler
             if (!uri.IsAbsoluteUri)
                 uri = new Uri(new Uri(baseUrl), uri);
             return uri.ToString();
+        }
+
+        public static List<string> RemoveDuplicatesSet(List<string> items)
+        {
+            var result = new List<string>();
+            var set = new HashSet<string>();
+            for (int i = 0; i < items.Count; i++)
+            {
+                if (!set.Contains(items[i]))
+                {
+                    result.Add(items[i]);
+                    set.Add(items[i]);
+                }
+            }
+            return result;
         }
     }
 }
